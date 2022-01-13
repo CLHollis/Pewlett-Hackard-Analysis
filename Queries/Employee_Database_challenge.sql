d6f0def0ead0ec824bcade2ec8f4e6863d97774f
Courtney Hollis <chollis1208@gmail.com>
2022-01-13 17:52:34 -0600

    last 2 Challenge questions

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -19,7 +19,7 @@ INNER JOIN titles as t ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
-SELECT * FROM retirement_titles
+SELECT COUNT (emp_no) FROM retirement_titles
 
 --  There are duplicate entries for some employees because they 
 --  have switched titles over the years. Use the following 
@@ -38,6 +38,7 @@ WHERE rt.to_date = ('9999-01-01')
 ORDER BY rt.emp_no, rt.to_date DESC;
 
 SELECT * FROM unique_titles
+SELECT COUNT (emp_no) FROM unique_titles
 
 -- Retrieve the number of employees by their most 
 -- recent job title who are about to retire.
@@ -47,7 +48,7 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY ut.count DESC;
 
-SELECT * FROM retiring_titles
+SELECT * FROM retiring_titles;
 
 ------ DELIVERABLE 2 -------
 
@@ -66,87 +67,36 @@ WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 	AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
 
-SELECT * FROM mentorship_eligibilty
+SELECT * FROM mentorship_eligibilty;
+SELECT COUNT (mentorship_eligibilty.emp_no) FROM mentorship_eligibilty;
 
+----------------------------------------------------------------------
 
+-- SUMMARY QUESTION 1 --
+-- What are the total salaries of retirees so the company knows how much 
+-- it can allocate to filling empty positions?
 
-
-
-
-
-
-
-
-EDITING
-
--- #1-7
-
-DROP TABLE retirement_titles;
-
-SELECT 	e.emp_no, e.first_name, e.last_name, 
-		t.title, t.from_date, t.to_date
-INTO retirement_titles
-FROM employees as e
-INNER JOIN titles as t ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY e.emp_no;
-
-SELECT * FROM retirement_titles;
-
--- #8-15
-DROP TABLE unique_titles;
-
-SELECT DISTINCT ON (rt.emp_no) 
-					rt.emp_no, 
-					rt.first_name, 
-					rt.last_name, 
-					rt.title
-INTO unique_titles
-FROM retirement_titles as rt
-WHERE rt.to_date = ('9999-01-01')
-ORDER BY rt.emp_no, rt.to_date DESC;
-
-SELECT * FROM unique_titles;
-
-----GOOG TIL HERE------------------------------------------
-
--- #16-21
--- 16.Write another query in the Employee_Database_challenge.sql file to 
---    retrieve the number of employees by their most recent job title who 
---    are about to retire.
-
-DROP TABLE retiring_titles;
-
--- 17. First, retrieve the number of titles from the Unique Titles table.
-SELECT COUNT (ut.first_name) AS "count", ut.title
---INTO retiring_titles
-FROM unique_titles as ut
-GROUP BY ut.title
-ORDER BY ut.count DESC;
-
-SELECT * FROM retiring_titles
-
-
-SELECT COUNT(first_name)   
-FROM employees as e   
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')  
-	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
------- DELIVERABLE 2 -------
-
--- Create a mentorship-eligibility table that holds the current 
--- employees who were born between 1/1/1965 and 12/31/1965.
-
-SELECT DISTINCT ON (e.emp_no)	
-		e.emp_no, e.first_name, e.last_name, e.birth_date, 
-		de.from_date, de.to_date,
-		t.title
-INTO mentorship_eligibilty
-FROM employees as e
+SELECT SUM (salary) 
+INTO total_salaries_retiring
+FROM salaries as s
+INNER JOIN employees as e ON (e.emp_no = s.emp_no)
 INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
-INNER JOIN titles as t ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01');
+
+SELECT * FROM total_salaries_retiring
+
+-- SUMMARY QUESTION 2 --
+-- Break that down by department.
+
+SELECT SUM (salary) AS "salaries", d.dept_name
+INTO total_salaries_retiring_by_dept
+FROM salaries as s
+INNER JOIN employees as e ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+INNER JOIN departments as d ON (de.dept_no = d.dept_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 	AND (de.to_date = '9999-01-01')
-ORDER BY e.emp_no;
+GROUP BY d.dept_name;
 
-SELECT * FROM mentorship_eligibilty
+SELECT * FROM total_salaries_retiring_by_dept
--- Queries/queries.sql
+++ Queries/queries.sql
@@ -101,6 +101,7 @@ LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
 
+--7.3.4--
 -- Create retirement_by_dept table for
 -- Employee count by department number
 SELECT COUNT(ce.emp_no), de.dept_no
@@ -116,14 +117,7 @@ SELECT * FROM retirement_by_dept
 SELECT * FROM salaries
 ORDER BY to_date DESC;
 
-SELECT emp_no, first_name, last_name, gender
-INTO emp_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT * FROM emp_info
-
+--7.3.5--
 SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
 INTO emp_info
 FROM employees as e
@@ -133,7 +127,7 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
      AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
      AND (de.to_date = '9999-01-01');
 	 
-SELECT * FROM current_emp
+SELECT * FROM emp_info
 
 -- List of managers per department
 SELECT  dm.dept_no,
@@ -163,8 +157,7 @@ INNER JOIN dept_emp AS de ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d ON (de.dept_no = d.dept_no)
 WHERE d.dept_name='Sales';
 
--- Include retirees from Sales & Development depts 
---   to create new Mentors Department
+-- Include retirees from Sales & Development depts to create new Mentors Department
 SELECT 	ce.emp_no, ce.first_name, ce.last_name, d.dept_name
 INTO mentor_info
 FROM current_emp as ce
